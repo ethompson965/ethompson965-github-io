d87931d53a9d4788120b30af409b9589cbb46c05
EC2 Default User <ec2-user@ip-172-31-83-3.ec2.internal>
2019-08-12 03:34:26 +0000

    update studies

--- studies/control-flow.js
+++ studies/control-flow.js
@@ -9,7 +9,7 @@
 * 1. Control flow executes a code from the first line in the file to the last line.
 * 2. Control flow is illustrated in conditional statements know as if-else
 */
-//Example
+//Example 
 
 var outside = 'hot'; // variable outside assigned to 'hot' to be executed in if-else chain
 if(outside === 'hot') { //first condition is met
@@ -18,4 +18,29 @@ if(outside === 'hot') { //first condition is met
      console.log('stay cool')
 } else {
     console.log('nevermine'); 
-} 
+}
+
+
+//Another conditional statement operated on control flow is a switch statement
+//switch statements replace multiple if checks in else if statements
+//switch statements allow you to select multiple blocks of code to execute
+//Example
+
+const action = 'say_hello';
+switch (action) {
+  case 'say_hello':
+    var message = 'hello';//'hello' is resolved in switch statement
+    console.log(message);
+    break;
+    case 'say_hello':
+    var message = 'Welcome'
+    console.log(message);//because of control flow Javascript ignores the second switch statement
+    break;
+  case 'say_hi':
+    var message = 'hi';
+    console.log(message);
+    break;
+  default:
+    console.log('Empty action received.');
+    break;
+}
--- studies/functions.js
+++ studies/functions.js
@@ -10,7 +10,16 @@
 followed by open and closed brackets. Inside the parenthesis identifiers known as parameters
 that just act as placeholders for a value. 
 */
-//Example function:
+/*2. Function parameters vs. function arguments 
+*Function parameters are names thathelp define the function you are creating but have no real 
+*value
+*You can name your function parameters anything but it's best to use names that correspond
+*with the function you're creating.
+*Function arguments are real values passed into the function and can manipulate the function 
+*value
+*/
+
+//3.  function syntax:
 function name(parameter) { //Declares a function named 'name' with placeholder 'parameter'
     
 }
@@ -25,6 +34,59 @@ function toCelsius(fahrenheit) { //Declares a function named to Celsius
     
 }
 toCelsius(95); // calls a function, printing to the console a value of 35
+//4. functions can be assigned to a variable by using a variable keyword to declare the name of the 
+//function.
+//Example
+var tocelsius = function(fahrenheit) { //declares function using keyword 'var'
+    return (5/9) * (fahrenheit-32)
+}
+//You can also use a variable to store the return value of a function
+//example
+var temp = toCelsius(95); 
+var text = "The temperature is " + temp + " Celsius"
+
+
+//5. You can optionally specify the inputs and outputs of a function by destructuring
+//Destructuring can assign default values in parameters and arguments
+//Example
+let myFunc = function({x = 5,y = 8,z = 13} = {}) {
+    console.log(x,y,z);
+};
+
+myFunc({y:15,x:10,a:1}); //10 15 13
+
+//6. Functions can see and modify variables in parent or global scopes 
+//Example
+// Initialize a global variable
+var species = "human";
+
+function transform() {
+  // Initialize a local, function-scoped variable
+  var species = "werewolf";
+  console.log(species);
+}
+
+// Log the global and local variable
+console.log(species);//human 
+transform();// werewolf
+console.log(species); //human
+/*7. Functions form closures around the data they house. If an object returned from the Function and is held in memory somewhere (referenced), 
+that closure stays ALIVE, and data can continue to exist in these closures*/
+//Example
+var add = (function () {
+  var counter = 0;
+  return function () {counter += 1; return counter}
+})();
+
+add();
+add();
+add();
+
+// the counter is now 3
+
+
+
+
 
 
 
